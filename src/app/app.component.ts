afa827ecc62cfcd2da77e1dc41f079222b46b9c0
Don Rogue <donrogue@protonmail.com>
2022-11-19 12:53:20 +0100

    #init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from 'src/app/models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'appOC';
+export class AppComponent implements OnInit{
+
+  arraySnaps!: FaceSnap[];
+
+  ngOnInit(){
+
+    this.arraySnaps = [
+      {
+        title : 'maki the goat',
+        date: new Date(),
+        imgUrl: 'https://ih1.redbubble.net/image.3304314408.5482/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        like: 44444,
+      },
+
+      {
+        title : 'toji fushiguro',
+        date : new Date(),
+        imgUrl : 'https://ih1.redbubble.net/image.4460561906.5650/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        like : 4444,
+      },
+
+      {
+        title : 'satoru gojo',
+        date : new Date(),
+        imgUrl : 'https://ih1.redbubble.net/image.4308192007.1487/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        like : 444,
+        location : 'Void'
+      }
+    ]
+
+  }
 }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,7 @@
+export class FaceSnap {
+  title! : string;
+  date!: Date;
+  imgUrl!: string;
+  like!:number;
+  location?: string;
+}
